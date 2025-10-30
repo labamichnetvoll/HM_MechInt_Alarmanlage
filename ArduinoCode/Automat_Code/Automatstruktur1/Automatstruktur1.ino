150a3c02e7d6485d613f90848610ada039674f79
Stefan Steyer <steyer.stefan@freenet.de>
2025-10-30 09:31:57 +0100

    SwitchCaseStruktur

--- ArduinoCode/Automat_Code/Automatstruktur1/Automatstruktur1.ino
+++ ArduinoCode/Automat_Code/Automatstruktur1/Automatstruktur1.ino
@@ -5,7 +5,7 @@ enum Zustand {
   Start,
   Initialisierung,
   Auswahl_Hotel,
-  Auswahl_Zeit,
+  Auswahl_Zelt,
   Auswahl_Gepaeck,
   Scharf_Hotel,
   Scharf_Gepaeck,
@@ -16,7 +16,7 @@ enum Zustand {
 
 
 void setup() {
-  
+  Zustand = Start;
 
 }
 
@@ -25,12 +25,37 @@ void loop() {
 
   if (millis() - lastupdate > PERIOD_UPDATE)  {
     //Zustandswechsel hier
+    switch(Zustand){
+    case Start:
+      break;
 
+    case Initialisierung:
+      break;
 
+    case Auswahl_Hotel:
+      break;
+
+    case Auswahl_Zelt:
+      break;
+
+    case Auswahl_Gepaek:
+      break;
+
+    case Scharf_Hotel:
+      break;
+
+    case Scharf_Gepaek:
+      break;
+
+    case Scharf_Zelt:
+      break;
+    
+    case Alarm:
+      break;
+      
+    }
     lastupdate = millis();
   }
-
-
 
 }
 
