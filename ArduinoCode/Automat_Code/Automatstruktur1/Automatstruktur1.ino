7b6733f3503d48423ec93262bd6861ac697e4b11
Labamichnetvoll <labamichnetvoll@gmail.com>
2025-11-10 19:16:53 +0100

    change/add: del ReadIMUAcc(), add #define SENSI, add Automat Weiter mit leben gefüllt (PinDefs, Alarm Logik)

--- ArduinoCode/Automat_Code/Automatstruktur1/Automatstruktur1.ino
+++ ArduinoCode/Automat_Code/Automatstruktur1/Automatstruktur1.ino
@@ -14,7 +14,12 @@
 /* PIN Nummer - Defintionen*/
 #define ULTRASONIC_PIN_NR 7     // evtl. anpassen! TODO   
 #define INFRAROT_PIN_NR 6       // evtl. anpassen! TODO
-#define BUTTON_PIN 2            // evtl. anpassen! TODO     Auswahl Pins: 2, 3, 9, 10, 11, 13, A1, A5, A7
+#define BUTTON_PIN 2            // evtl. anpassen! TODO     Auswahl Pins Interrupt: 2, 3, 9, 10, 11, 13, A1, A5, A7
+#define LED_PIN 4               // evtl. anpassen! TODO
+#define PIEZZO_PIN 5            // evtl. anpassen! TODO
+#define PWM_PIN 9               // evtl. anpassen! TODO     Wichtig PWM PIN erwischen
+#define SHDN_PIN 10             // evtl. anpassen! TODO     Verstärker AN/AUS
+#define MUTE_PIN 11             // evtl. anpassen! TODO     Verstärker Mute  
 
 /* Konstanten */
 #define PERIOD_UPDATE 100   // Taktlänge in ms
@@ -30,15 +35,21 @@ enum enZustaende {
 };
 enum enZustaende Zustand;
 
-/* Sensorvariablen */
+/* globale Sensorvariablen , werden durch WebUI aktiviert*/
 bool bIR_Sensor_an = 0;
 bool bUS_Sensor_an = 0;
 bool bA_Sensor_an = 0;
 bool bSensor_ausgeloest = 0;
 
+/* globale Sensorvariable, codeintern*/
+long ldistultraschallvgl = 0;
+
+
 /* Konstruktor CPP Klassen*/
 Ultrasonic ultrasonic(ULTRASONIC_PIN_NR);
 
+/* Funktionsprototypen*/
+void Alarm();
 
 void setup() {
 
@@ -49,6 +60,12 @@ void setup() {
   pinMode(INFRAROT_PIN_NR, INPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   //attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), ISR_NAME??, CHANGE);   //ISR Funktion hinzufügen
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(PIEZZO_PIN, OUTPUT);
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(SHDN_PIN, OUTPUT);      
+
+  digitalWrite(SHDN_PIN, 0);    // Deaktiviert Verstärker Modul
 
   /* IMU Init*/
   if (!IMU.begin()){
@@ -58,6 +75,14 @@ void setup() {
   Serial.print("Accelerometer sample rate = ");
   Serial.print(IMU.accelerationSampleRate());
   Serial.println(" Hz");
+
+  //Test ob Ultraschall Sensor angeschlossen und rdy
+  if (ReturnUltraschall() == 0){
+    Serial.println("Ultraschall defekt");
+  }
+
+
+  // Lautsprecher Konfig
 
 
 
@@ -69,67 +94,119 @@ void setup() {
 
 
 void loop() {
+  
 
   if (millis() - ulLastupdate > PERIOD_UPDATE)  {   //if-Abfrage funktioniert für mindestens 7 Wochen, dann Overflow möglich
 
     ulLastupdate = millis();  //Taktzähler zurücksetzen
 
-    RawIMU();
+    ReturnAcceleration();   //TEST
+
 
     //Zustandswechsel hier
     switch(Zustand){
 
-    case Start:
-      //Sensoren und Aktoren Initialisieren
-      bIR_Sensor_an = 0;
-      bUS_Sensor_an = 0;
-      bA_Sensor_an = 0;
-      Zustand = Sensorauswahl;
-      break;
-    
-    case Sensorauswahl:
-      //Diplay zeigt "Bereit"
-      //Web UI kann Sensorvariablen ändern:
-      if(bIR_Sensor_an || bUS_Sensor_an || bA_Sensor_an){
-        Zustand = Aktiv;
-      }
-      break;
+      case Start:
+        // Alarm ausschalten
+        analogWrite(PWM_PIN, 0);
+        digitalWrite(PWM_PIN, 0); // safety first
+        digitalWrite(SHDN_PIN, 0);      // Verstärker AUS
+        //Sensoren und Aktoren Initialisieren
+        bIR_Sensor_an = 0;
+        bUS_Sensor_an = 0;
+        bA_Sensor_an = 0;
+        Zustand = Sensorauswahl;
+        break;
+      
+      case Sensorauswahl:
+        //Diplay zeigt "Bereit"
+        // (Sensorvariablen lesen aus WebUI änderungen):
+        if(bIR_Sensor_an || bUS_Sensor_an || bA_Sensor_an){
+          ldistultraschallvgl = ReturnUltraschall();
+          Zustand = Aktiv;
+        }
 
-    case Aktiv:
-      if(bIR_Sensor_an){
-        //Sensor abfragen
-      }
-      if(bUS_Sensor_an){
-        //Sensor abfragen
-      }
-      if(bA_Sensor_an){
-        //Sensor abfragen
-      }
+        
+        break;
 
-      if(bSensor_ausgeloest){
-        Zustand = Alarm;
-      }
-      break;
+      case Aktiv:
+        if(bIR_Sensor_an){
+          //Sensor abfragen
+          bSensorAusgeloest = ReturnInfrarot();
 
-    case Alarm:
-      //LED blinken, Lautsprecher aktivieren, Piezo aktivieren
-      //Web UI kann bSensor_ausgeloest auf 0 setzen
+        }
+        if(bUS_Sensor_an){
+          //Sensor abfragen & auswerten
+          if ( ReturnUltraschall() - ldistultraschallvgl > 5 || ReturnUltraschall() - ldistultraschallvgl < -5 )  {
+            bSensor_ausgeloest = 1;
+          }
+          
+        }
+        if(bA_Sensor_an){
+          //Sensor abfragen
+          bSensor_ausgeloest = ReturnAcceleration();
+        }
 
-      if(!bSensor_ausgeloest){
+        if(bSensor_ausgeloest){
+          Zustand = Alarm;
+        }
+        break;
+
+      case Alarm:
+        //LED blinken, Lautsprecher aktivieren, Piezo aktivieren
+        //Web UI kann bSensor_ausgeloest auf 0 setzen
+
+        Alarm();
+
+
+        if(!bSensor_ausgeloest){
+          Zustand = Start;
+        }
+        break;
+
+      default:
+        //Nur zur Fehlerbehebung, immer zu Start wechseln
         Zustand = Start;
+        break;
       }
-      break;
-
-    default:
-      //Nur zur Fehlerbehebung, immer zu Start wechseln
-      Zustand = Start;
-      break;
-    }
   }
-  if (ulLastupdate - millis() > 0) {    //Bei Overflow: ulLastupdate neu setzen, um Takt wiederherzustellen
+    
+    
+  if (ulLastupdate > millis() ) {    //Bei Overflow: ulLastupdate neu setzen, um Takt wiederherzustellen
     ulLastupdate = millis();
   }
+  
+
 }
 
 // Funktionen hier
 
+//LED blinken, Lautsprecher aktivieren, Piezo aktivieren
+void Alarm(){
+
+  volatile unsigned long takt = 0;
+  digitalWrite(SHDN_PIN, 1);      // Verstärker AN
+
+  if (takt % 2) digitalWrite(PIEZZO_PIN, 1);   // PIEZZEO AN
+  else digitalWrite(PIEZZO_PIN, 0);   // PIEZZEO AUS
+
+  
+  analogWrite(PWM_PIN, 50);      // 0-255: mitte am lautesten
+ 
+
+  if (takt < 20){
+    digitalWrite(LED_PIN, 1);   // LED AN
+    
+  }
+  else if (takt < 40){
+    digitalWrite(LED_PIN, 0);   // LED AUS    
+
+  }
+  else {
+    takt = 0;
+  }
+  
+  takt++;
+
+
+}
--- ArduinoCode/Automat_Code/Automatstruktur1/Sensorik.ino
+++ ArduinoCode/Automat_Code/Automatstruktur1/Sensorik.ino
@@ -5,19 +5,18 @@
  * https://docs.arduino.cc/tutorials/nano-33-iot/imu-accelerometer/
 */
 
+#define SENSI 0.01
 
 float acc_x, acc_y, acc_z;    //Speicher für Beschleunigungswerte
 float acc_x_old, acc_y_old, acc_z_old;  
-int degreesX = 0;
-int degreesY = 0;
-int degreesZ = 0;      
+  
 
 /**
  * @brief Gibt die gemessene Entfernung in cm zurück
 */
 long ReturnUltraschall()  {
   //Drift berücksichtigen!!!
-  return ultrasonic.MeasureInCentimeters();   //
+  return ultrasonic.MeasureInCentimeters(/*TimeOUT in microsek */ );   // Für Initialisierung
 
 }
 
@@ -32,90 +31,37 @@ int ReturnInfrarot(int pin_nr) {
 
 /**
  * @brief Ließt Beschleunigungssensor aus
- * Rückgabewert: bool ( Erfolgreich gelesen ) 0: nein, 1: Erfolg
+ * Rückgabe:  1 bei Bewegung erkannt,  0 wenn keine Bewegung erkannt
  *
- * TODO: evtl mit Parametern aufrufen und über Pointer werte Übergeben. alternativ nur Ausgelöst ja/nein?
+ * @TODO: Sensitivität kontrollieren & anpassen?!
 */
-int ReadIMUAcc() {
-  bool dataread;
-  if (IMU.accelerationAvailable()) {
-    dataread = IMU.readAcceleration(acc_x, acc_y, acc_z);
-
-  }
-  else{
-    return 0;
-  }
-
-/* Test Code --> IMU Verifizieren*/
-  if (acc_x > 0.2) {
-    acc_x = 100 * acc_x;
-    degreesX = map(acc_x, 0, 97, 0, 90);
-    Serial.print("Tilting up ");
-    Serial.print(degreesX);
-    Serial.println("  degrees");
-  }
-  if (acc_x < -0.2) {
-    acc_x = 100 * acc_x;
-    degreesX = map(acc_x, 0, -100, 0, 90);
-    Serial.print("Tilting down ");
-    Serial.print(degreesX);
-    Serial.println("  degrees");
-  }
-  if (acc_y > 0.2) {
-    acc_y = 100 * acc_y;
-    degreesY = map(acc_y, 0, 97, 0, 90);
-    Serial.print("Tilting left ");
-    Serial.print(degreesY);
-    Serial.println("  degrees");
-  }
-  if (acc_y < -0.2) {
-    acc_y = 100 * acc_y;
-    degreesY = map(acc_y, 0, -100, 0, 90);
-    Serial.print("Tilting right ");
-    Serial.print(degreesY);
-    Serial.println("  degrees");
-  }
-  if (acc_z > 0.2) {
-    acc_z = 100 * acc_z;
-    degreesZ = map(acc_z, 0, 97, 0, 90);
-    Serial.print("Tilting front? ");
-    Serial.print(degreesZ);
-    Serial.println("  degrees");
-  }
-  if (acc_z < -0.2) {
-    acc_z = 100 * acc_z;
-    degreesZ = map(acc_z, 0, -100, 0, 90);
-    Serial.print("Tilting back? ");
-    Serial.print(degreesZ);
-    Serial.println("  degrees");
-  }
-
-  return dataread;
-}
-
-// Test Funktion
-void RawIMU() {
+bool ReturnAcceleration() {
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(acc_x, acc_y, acc_z);
-  }
 
-  if (acc_x - acc_x_old > 0.01) {
-    Serial.print("X:");
-    Serial.println(acc_x);
-    acc_x_old = acc_x;
-  }
-   if (acc_y - acc_y_old > 0.01) {
-    Serial.print("Y:");
-    Serial.println(acc_y);
-    acc_y_old = acc_y;
-  }
-   if (acc_z - acc_z_old > 0.01) {
-    Serial.print("Z:");
-    Serial.println(acc_z);
-    acc_z_old = acc_z;
-  }
+    if (acc_x - acc_x_old > SENSI || acc_x - acc_x_old < -SENSI) {
+      Serial.print("X:");
+      Serial.println(acc_x, 5);
+      acc_x_old = acc_x;
+      return 1;
+    }
+    if (acc_y - acc_y_old > SENSI || acc_y - acc_y_old < -SENSI) {
+      Serial.print("Y:");
+      Serial.println(acc_y, 5);
+      acc_y_old = acc_y;
+      return 1;
+    }
+    if (acc_z - acc_z_old > SENSI || acc_z - acc_z_old < -SENSI) {
+      Serial.print("Z:");
+      Serial.println(acc_z, 5);
+      acc_z_old = acc_z;
+      return 1;
+    }
 
+  }
+  else{ Serial.println("WTF");}
 
+  return 0;
 }
 
 
